2853bbba2b8fe1b39127cb91c56a78e15f1e193b
Ozan Safi <ozan.safi@klarna.com>
2016-08-03 11:30:35 +0200

    docker: take password in an env var, if available
    Signed-off-by: Ozan Safi <ozan.safi@klarna.com>

--- docker_build/start.sh
+++ docker_build/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-echo -e "op5_url: https://monitor.int.klarna.net/api\nop5_username: $op5_username" > /root/.config/op5-cli/config.yaml
+echo -e "op5_url: https://monitor.int.klarna.net/api\nop5_username: $op5_username\n" > /root/.config/op5-cli/config.yaml
+if [ ! -z $op5_password ]; then
+	echo -e "op5_password: $op5_password\n" >> /root/.config/op5-cli/config.yaml
+fi
 /usr/bin/op5-cli $*
