13a9f5adaef1bbbe3185646987b9ebbbbf34cbd4
Ozan Safi <ozan.safi@klarna.com>
2016-08-22 15:19:14 +0200

    pkg: add MANIFEST.in and require op5lib

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 import site
-import os
-curdir = os.getcwd()
-
-from op5lib import setup
-
-os.chdir(curdir)
 
 try:
     from setuptools import setup
@@ -13,12 +7,12 @@ except ImportError:
 
 setup(
     name='op5-cli',
-    version='1.0',
+    version='1.0.4',
     author='Ozan Safi',
     author_email='ozansafi@gmail.com',
     scripts=['op5-cli'],
     data_files=[(site.getuserbase()+"/man/man1/", ['man/op5-cli.1'])],
-    url='https://github.com/ozans/op5-cli',
+    url='https://github.com/klarna/op5-cli',
     license='LICENSE.txt',
     description='A command-line interface for the OP5 monitoring system',
     long_description=open('README.txt').read(),
@@ -27,6 +21,7 @@ setup(
         "argparse>=1.2.1",
         "requests>=2.3.0",
         "termcolor>=1.1.0",
-        "keyring>=5.4"
+        "keyring>=5.4",
+        "op5lib==1.0"
     ],
 )
